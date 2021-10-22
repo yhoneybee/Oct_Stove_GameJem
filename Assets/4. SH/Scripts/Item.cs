9aaca621f26d2a1d96222d4da8130ffbfb3696ff
Yoon-soo-Hyun <pzxcqwsx2@gmail.com>
2021-10-23 02:15:21 +0900

    Inven Scene/InvenMgr/Item/ItemDatabase [Create]

---
+++ Assets/4. SH/Scripts/Item.cs
@@ -0,0 +1,17 @@
+public class Item
+{
+    public string itemName;
+    public int itemIndex;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

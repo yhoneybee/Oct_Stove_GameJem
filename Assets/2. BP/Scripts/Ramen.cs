55bf3b11f1420cc43f9cfa9b70c630345b8f1c03
Black_Penguin <dkstmdwnswns@gmail.com>
2021-10-23 12:03:45 +0900

    Ramen Complete

--- Assets/2. BP/Scripts/Ramen.cs
+++ Assets/2. BP/Scripts/Ramen.cs
@@ -5,11 +5,36 @@ using UnityEngine.UI;
 
 public class Ramen : MonoBehaviour
 {
-    public List<Button> Ramens;
-    bool color;
-    
-    void ClickEvent()
+    [SerializeField] List<Image> sprites;
+    bool[] color = { false, false,false,true};
+    private void Start()
     {
-        
+
+    }
+
+    private void Update()
+    {
+        for(int i = 0; i < 4; i++)
+        {
+            if (color[i] == false)
+            {
+                sprites[i].color = new Color(1, 0, 0);
+            }
+            else
+                sprites[i].color = new Color(0, 1, 0);
+        }
+    }
+    public void ClickEvent(int number)
+    {
+        if (color[number] == true)
+        {
+            color[number] = false;
+        }
+        else
+            color[number] = true;
+        if (color[0] == false && color[1] == true && color[2] == false && color[3] == false)
+        {
+            Debug.Log("정답이네요!");
+        }
     }
 }

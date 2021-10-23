00005336499d93e343671c6052563a9e98e36e5a
daeho0818 <daeho0818@naver.com>
2021-10-23 15:36:32 +0900

    버튼회전함히히

--- Assets/Main/Scripts/UI/IngameUI.cs
+++ Assets/Main/Scripts/UI/IngameUI.cs
@@ -17,12 +17,25 @@ public class IngameUI : MonoBehaviour
     [SerializeField] Button side_ui_button;
 
     RectTransform side_ui_button_transform;
+    Coroutine rotation_coroutine = null;
+    Coroutine onoff_coroutine = null;
 
     int current_count = 0;
     bool is_side_ui_appeard = true;
     void Start()
     {
-        side_ui_button.onClick.AddListener(() => { StartCoroutine(OnOffSideUI()); });
+        side_ui_button.onClick.AddListener(() =>
+        {
+            if (rotation_coroutine != null)
+                StopCoroutine(rotation_coroutine);
+            
+            rotation_coroutine = StartCoroutine(RotationButton());
+
+            if (onoff_coroutine != null)
+                StopCoroutine(onoff_coroutine);
+
+            onoff_coroutine = StartCoroutine(OnOffSideUI());
+        });
 
         side_ui_button_transform = side_ui_button.GetComponent<RectTransform>();
 
@@ -32,7 +45,6 @@ public class IngameUI : MonoBehaviour
 
     void Update()
     {
-        side_ui_button_transform.rotation = Quaternion.Lerp(side_ui_button_transform.rotation, Quaternion.Euler(0, 0, !is_side_ui_appeard ? 180 : 0), 0.5f);
 
         hair_count_text.text = $"{StageManager.Instance.player_information.HairCount} / {StageManager.Instance.need_hair_count}";
     }
@@ -43,6 +55,18 @@ public class IngameUI : MonoBehaviour
         timer_text.text = $"{current_count / 60} : {current_count % 60}";
     }
 
+    IEnumerator RotationButton()
+    {
+        bool is_side_ui_appeard = this.is_side_ui_appeard;
+
+        while (Quaternion.Angle(side_ui_button_transform.rotation, Quaternion.Euler(0, 0, is_side_ui_appeard ? 180 : 0)) > 0.1f)
+        {
+            side_ui_button_transform.rotation = Quaternion.Lerp(side_ui_button_transform.rotation, Quaternion.Euler(0, 0, is_side_ui_appeard ? 180 : 0), 0.5f);
+            yield return new WaitForSeconds(0.01f);
+        }
+
+        rotation_coroutine = null;
+    }
     IEnumerator OnOffSideUI()
     {
         while (Vector2.Distance(side_ui.position, is_side_ui_appeard ? target_transform.position : current_transform.position) > 0.1f)
@@ -51,5 +75,7 @@ public class IngameUI : MonoBehaviour
             yield return new WaitForSeconds(0.01f);
         }
         is_side_ui_appeard = !is_side_ui_appeard;
+
+        onoff_coroutine = null;
     }
 }

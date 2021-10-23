05c8f52aebc18a051823b7d94b8f1fe00b06d49b
daeho0818 <daeho0818@naver.com>
2021-10-23 12:55:07 +0900

    UI 기능 싹다 완성

--- Assets/Main/Scripts/UI/IngameUI.cs
+++ Assets/Main/Scripts/UI/IngameUI.cs
@@ -2,9 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class IngameUI : MonoBehaviour
 {
+    [SerializeField] TextMeshProUGUI hair_count_text;
+    [SerializeField] TextMeshProUGUI timer_text;
+
     [SerializeField] RectTransform side_ui;
 
     [SerializeField] RectTransform current_transform;
@@ -14,18 +18,31 @@ public class IngameUI : MonoBehaviour
 
     RectTransform side_ui_button_transform;
 
+    int current_count = 0;
     bool is_side_ui_appeard = true;
     void Start()
     {
         side_ui_button.onClick.AddListener(() => { StartCoroutine(OnOffSideUI()); });
 
         side_ui_button_transform = side_ui_button.GetComponent<RectTransform>();
+
+        current_count = StageManager.Instance.timer;
+        InvokeRepeating("CountDown", 0, 1);
     }
 
     void Update()
     {
         side_ui_button_transform.rotation = Quaternion.Lerp(side_ui_button_transform.rotation, Quaternion.Euler(0, 0, !is_side_ui_appeard ? 180 : 0), 0.5f);
+
+        hair_count_text.text = $"{StageManager.Instance.player_information.HairCount} / {StageManager.Instance.need_hair_count}";
     }
+    void CountDown()
+    {
+        current_count--;
+
+        timer_text.text = $"{current_count / 60} : {current_count % 60}";
+    }
+
     IEnumerator OnOffSideUI()
     {
         while (Vector2.Distance(side_ui.position, is_side_ui_appeard ? target_transform.position : current_transform.position) > 0.1f)

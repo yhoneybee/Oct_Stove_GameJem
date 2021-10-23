24f1d6f849eaba0d26672b4eeda8150b13ea3133
daeho0818 <daeho0818@naver.com>
2021-10-23 15:40:45 +0900

    ㅁㄴㅇㄹ

--- Assets/Main/Scripts/Items/InteractiveObject.cs
+++ Assets/Main/Scripts/Items/InteractiveObject.cs
@@ -6,12 +6,12 @@ using UnityEngine.UI;
 public abstract class InteractiveObject : MonoBehaviour
 {
     public Image image;
-    public Sprite image_sprite;
 
     Button button;
 
     public int ID;
-    public bool condition;
+
+    protected bool condition;
     protected virtual void Start()
     {
         image = GetComponent<Image>();

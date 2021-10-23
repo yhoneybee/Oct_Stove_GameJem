c67263e14fcb807d0056d7265872e0240532f0cc
daeho0818 <daeho0818@naver.com>
2021-10-23 13:24:03 +0900

    서랍장 열었다 닫았다..

--- Assets/Main/Scripts/Items/Stage 1/Stage1_Object_Button.cs
+++ Assets/Main/Scripts/Items/Stage 1/Stage1_Object_Button.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class Stage1_Object_Button : InteractiveObject
 {
     [SerializeField] Stage1_Object_Can object_can;
+
+    // 서랍 열었을 때 보이는 아이콘들 (창에 있는애 아님)
+    [SerializeField] GameObject can_icon;
+    [SerializeField] GameObject button_icon;
     protected override void Action()
     {
         StageManager.Instance.player_information.object_id_arr[ID] = 1;
@@ -12,5 +16,8 @@ public class Stage1_Object_Button : InteractiveObject
 
         StageManager.Instance.player_information.object_id_arr[object_can.ID] = 1;
         object_can.gameObject.SetActive(false);
+
+        can_icon.SetActive(false);
+        button_icon.SetActive(false);
     }
 }
--- Assets/Main/Scripts/Items/Stage 1/Stage1_Object_Drawer.cs
+++ Assets/Main/Scripts/Items/Stage 1/Stage1_Object_Drawer.cs
@@ -4,7 +4,12 @@ using UnityEngine;
 
 public class Stage1_Object_Drawer : InteractiveObject
 {
-    [SerializeField] GameObject Drawer;
+    // 서랍 창
+    [SerializeField] GameObject drawer;
+    // 서랍 열었을 때 이동시킬 위치
+    [SerializeField] RectTransform target_transform;
+
+    bool is_opened = false;
     protected override void Start()
     {
         base.Start();
@@ -13,6 +18,28 @@ public class Stage1_Object_Drawer : InteractiveObject
     }
     protected override void Action()
     {
-        Drawer.SetActive(true);
+        StartCoroutine(Animation());
+    }
+
+    IEnumerator Animation()
+    {
+        yield return StartCoroutine(MoveDrawer());
+
+        drawer.SetActive(true);
+    }
+    IEnumerator MoveDrawer()
+    {
+        RectTransform rt = GetComponent<RectTransform>();
+        while (Vector2.Distance(rt.position, target_transform.position) > 0.1f)
+        {
+            rt.position = Vector2.Lerp(rt.position, target_transform.position, 0.5f);
+            yield return new WaitForSeconds(0.01f);
+        }
+
+        if (!is_opened)
+        {
+            is_opened = true;
+            yield return new WaitForSeconds(1);
+        }
     }
 }

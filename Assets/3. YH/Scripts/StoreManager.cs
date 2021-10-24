3e6c4ce0e2c6fbde853889aa4dd16b9890c3f4b1
kkulbeol <chaeyh04@gmail.com>
2021-10-24 12:20:56 +0900

    UPDATE * Store

---
+++ Assets/3. YH/Scripts/StoreManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class StoreManager : MonoBehaviour
+{
+    public Button Button;
+    public List<Sprite> sprites = new List<Sprite>();
+    public Image Img;
+
+    bool isButtonGotten = false;
+
+    private void Start()
+    {
+        Button.onClick.AddListener(() =>
+        {
+            if (isButtonGotten)
+            {
+                Img.sprite = sprites[1];
+                StageManager.Instance.player_information.HairCount++;
+                StartCoroutine(EChangeSprite());
+            }
+            else
+            {
+                Img.sprite = sprites[0];
+                GetComponent<RectTransform>().anchoredPosition = new Vector2(-414, 168.7f);
+                isButtonGotten = true;
+            }
+        });
+    }
+
+    IEnumerator EChangeSprite()
+    {
+        yield return StartCoroutine(UIManager.Instance.EColoringUI(Img, Color.black, 3));
+        Img.sprite = sprites[2];
+        yield return StartCoroutine(UIManager.Instance.EColoringUI(Img, Color.white, 3));
+    }
+}
